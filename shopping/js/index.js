f7cdf4ca4b15175e79cf4110b14c413da139f138
eblis..e.a.zhao <eblis.e.a.zhao@gmail.com>
2014-04-08 00:42:36 +0800

    shopping website index
    version 0.7.1

--- shopping/js/index.js
+++ shopping/js/index.js
@@ -1,19 +1,25 @@
 // JavaScript Document
 // multinavbar
 $(function(){
+	function clearSelected() {
+		$(".multinav_bar li").removeClass("selected");
+		$(".multinav_tips .tip").hide();
+	}
+	// $(".multinav_bar").on("mouseleave", clearSelected);
 	$(".multinav_bar li").each(function (index, element) {
-        $(this).on("mouseout", function (e){
-			$(this).removeClass("selected");
-			$(".multinav_tips .tip:eq(" + index + ")").hide();
-		});
 		$(this).on("mouseover", function (e){
 			var obj=$(this).offset(); 
-			var xobj=obj.left + 160 + "px"; 
+			var xobj=obj.left + 168 + "px"; 
 			var yobj=obj.top - 1 + "px"; 
+			clearSelected();
 			$(this).addClass("selected");
 			$(".multinav_tips .tip:eq(" + index + ")").css({"left":xobj,"top":yobj}).show();
 		});
     });
+	$(".multinav_tips .tip").each(function(index, element) {
+        $(this).on("mouseleave", clearSelected);
+    });
+	
 });
 
 // newsmodule_label event
@@ -22,8 +28,9 @@ $(function(){
         $(this).on("mouseover", function (e){
 			$(this).siblings().removeClass("selected");
 			$(this).addClass("selected");
-			$(".newsmodule_content").fadeOut(100);	
-			$(".newsmodule_content:eq(" + index + ")").fadeIn(100);
+			$(".newsmodule_content").hide(100);	
+			setTimeout(function (){ $(".newsmodule_content:eq(" + index + ")").show(100);}, 100);
+			nwscli_init(index);
 		});
     });
 	
