cda669e3a5f70af10625de42284d8dbbbab1e5eb
zhaoweih <zhaowih@chanjet.com>
2014-06-14 17:28:28 +0800

    iov100
    header upload
    qr still error

--- carNetWeb/WebRoot/js/scroll.js
+++ carNetWeb/WebRoot/js/scroll.js
@@ -1,18 +1,15 @@
 // JavaScript Document
 var temp = 0;
 $(document).ready(function(e) {
-    var $qh = $("#qr_float").prev();
-	temp = $qh.position().top + $qh.height();
-	console.log(temp);
+	temp = $("#qr_float").offset().top;
 });
 
 $(window).on("scroll", function(e){
 	// header 
 	var offset = $(window).scrollTop();
-	console.log("offset is" + offset);
 	if (offset > 57) {
 		$("#header").addClass("header_float");
-		$(".main").css("margin-top", "150px");
+		$(".main").css("margin-top", "207px");
 	} else {
 		$("#header").removeClass("header_float");
 		$(".main").css("margin-top", "40px");
